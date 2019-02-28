fa66234e69c6e03df37b34db10ebbdfc5684c6eb
camfeghali <camfeg@gmail.com>
2019-02-28 11:30:06 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
+    # byebug
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
